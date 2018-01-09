4d79e59bfa56544b66afa6e94b363400b394f8db
Bartek Buslowicz <bartek@burin.pl>
2018-01-09 12:39:52 +0100

    Add next project

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+// var url = 'https://pokeapi.co/api/v2/pokemon/';
+// var pokemonList = $('#pokemons');
+// var grid = $('#grid');
+// $("button").click(function(){
+// 	$.ajax({
+// 		url: url,
+// 		method: 'GET',
+// 		success: showPokemonList
+// 	});
+// });
+// function showPokemonList(resp) {
+// 	console.log(resp.results);
+// 	resp.results.forEach(function(item, name) {
+// 		// $('<li>').text(item.name).appendTo(pokemonList);
+// 		// $('<div class="col-3" id="grid2">').text(item.name).appendTo(grid);
+// 		$('<img class="col-1" src="https://pokeapi.co/media/img/'+ name +'.png">').appendTo(grid);
+// 		// var para = $('#grid');
+// 		// para.each(function(index, element) {
+// 		// 	var img = '<img class="col-3" src="https://pokeapi.co/media/img/' + index + '.png">'
+//   //  			$(element).append(img)
+// 		// });
+// 		})
+// 	url = resp.next;
+// };
 var url = 'https://pokeapi.co/api/v2/pokemon/';
 var pokemonList = $('#pokemons');
 $("button").click(function(){
@@ -11,6 +35,8 @@ function showPokemonList(resp) {
 	console.log(resp.results);
 	resp.results.forEach(function(item, name) {
 		$('<li>').text(item.name).appendTo(pokemonList);
+		count = name + 1;
+		$('<img class="col-1" src="https://pokeapi.co/media/img/'+ count +'.png">').appendTo(pokemonList);
 		})
 	url = resp.next;
 };
